8ef26ead60b9c091b954ea62146932ae7ddd081e
iuh20113401 <kiet5454aa@gmail.com>
2024-06-10 15:46:55 +0700

    first commit

--- src/accounts/AccountOperations.js
+++ src/accounts/AccountOperations.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { batch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deposit, loanRequest, payLoan, withdraw } from "./AccountSlice";
 
 function AccountOperations() {
